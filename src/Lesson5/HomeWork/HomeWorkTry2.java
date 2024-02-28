6e9d58b9a6834107ac9b17b9d9a93a81780929d0
JackCayman <mygamemylive@gmail.com>
2024-02-28 19:28:30 +0300

    dwa

--- src/Lesson5/HomeWork/HomeWorkTry2.java
+++ src/Lesson5/HomeWork/HomeWorkTry2.java
@@ -35,34 +35,29 @@ public class HomeWorkTry2 {
         System.arraycopy(arr, 0, leftHalf, 0, arr.length / 2);
         System.arraycopy(arr, arr.length / 2, rightHalf, 0, arr.length / 2);
 // Запускает два потока и параллельно просчитываем каждый малый массив
-       Thread thread1 = new Thread(() -> {
+        Thread thread1 = new Thread(() -> {
             for (int i = 0; i < leftHalf.length; i++) {
                 leftHalf[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
-      Thread thread2 =  new Thread(() -> {
+        Thread thread2 = new Thread(() -> {
             for (int i = 0; i < rightHalf.length; i++) {
                 rightHalf[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
-      thread1.start();
-      thread2.start();
+        thread1.start();
+        thread2.start();
         try {
             thread1.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        try {
             thread2.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
 // Склеиваем малые массивы обратно в один большой
         float[] mergetArr = new float[arr.length];
         System.arraycopy(leftHalf, 0, mergetArr, 0, arr.length / 2);
         System.arraycopy(rightHalf, 0, mergetArr, arr.length / 2, arr.length / 2);
-        System.out.println("Two thread time: " + (System.currentTimeMillis() -
-                startTime) + " ms.");
+        System.out.println("Two thread time: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
-
 }
